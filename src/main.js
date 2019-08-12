8de30e39ee8e8d60d4cafa84d32245ac8e681596
root <root@localhost.localdomain>
2019-08-12 18:57:27 +0800

    checking ui

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import {getSearch} from "./components/search.js";
 import {getProfile} from "./components/profile.js";
+import {getMenu} from "./components/menu.js";
+import {getStatistic} from "./components/statistic.js";
+import {getFilmLayout} from "./components/film-layout.js";
+import {getFilmCard} from "./components/film-card.js";
+import {getFilmPopup} from "./components/film-popup.js";
+import {getSortBar} from "./components/sort-bar.js";
 
 const header = document.querySelector(`.header`);
+const main = document.querySelector(`.main`);
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
@@ -9,3 +16,26 @@ const render = (container, template, place = `beforeend`) => {
 
 render(header, getSearch());
 render(header, getProfile());
+
+render(main, getMenu());
+render(main, getSortBar());
+render(main, getStatistic());
+render(main, getFilmLayout());
+
+const filmList = document.querySelector(`.films-list__container`);
+
+const filmListTopRated = document.querySelector(
+    `.films-list--extra:nth-of-type(1) .films-list__container`
+);
+
+const filmListMostCommented = document.querySelector(
+    `.films-list--extra:nth-of-type(2) .films-list__container`
+);
+
+new Array(5).fill(``).forEach(() => render(filmList, getFilmCard()));
+new Array(2).fill(``).forEach(() => render(filmListTopRated, getFilmCard()));
+new Array(2)
+  .fill(``)
+  .forEach(() => render(filmListMostCommented, getFilmCard()));
+
+// render(main, getFilmPopup());
