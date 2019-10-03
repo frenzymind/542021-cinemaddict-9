180d7719d0c6121ecb5032bd918850202179230b
root <root@localhost.localdomain>
2019-10-03 18:45:13 +0800

    sort data attribute

--- src/components/sort-bar.js
+++ src/components/sort-bar.js
@@ -7,9 +7,9 @@ export class SortBar extends AbstractComponent {
 
   getTemplate() {
     return `<ul class="sort">
-  <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-  <li><a href="#" class="sort__button">Sort by date</a></li>
-  <li><a href="#" class="sort__button">Sort by rating</a></li>
+  <li><a href="#" data-sort-method="default" class="sort__button sort__button--active">Sort by default</a></li>
+  <li><a href="#" data-sort-method="date" class="sort__button">Sort by date</a></li>
+  <li><a href="#" data-sort-method="rate" class="sort__button">Sort by rating</a></li>
 </ul>`;
   }
 }
